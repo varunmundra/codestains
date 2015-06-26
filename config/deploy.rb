6ce805c8cd7abf68c61439e17e3da3299217c21d
Varun <varunmundra.iitkgp@gmail.com>
2015-06-26 18:43:04 +0530

    asss

--- config/deploy.rb
+++ config/deploy.rb
@@ -72,10 +72,10 @@ task :deploy => :environment do
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
  
-    # to :launch do
-    #   queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
-    #   queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
-    # end
+    to :launch do
+      queue "restart codestains"
+      
+    end
   end
 end
 
